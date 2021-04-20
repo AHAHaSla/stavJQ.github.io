9c82552a9abb5a8ea9e453572e388fe8c7e0047a
AHAHaSla <34772252+AHAHaSla@users.noreply.github.com>
2021-04-20 15:42:40 +0300

    Add files via upload

---
+++ js/jq.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".box").mouseenter(function (){
+        $(this).toggle('red');
+        //alert('UvU');
+    })
+    //$(".box").mouseleave(function (){
+    //    $(this).toggle('red');
+    //    //alert('UvU');
+    //})
+});
